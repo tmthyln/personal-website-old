2fcf76a86add166d88cdbc45e50b3356f962a11e
Tim Lin <tmthylin@gmail.com>
2019-11-14 01:47:26 -0500

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # abort on errors
-set -e
+#set -e
 
 # build
 npm run build
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push git@github.com:tmthyln/tmthyln.github.io.git master
+git push -f git@github.com:tmthyln/tmthyln.github.io.git master
 
 cd -
